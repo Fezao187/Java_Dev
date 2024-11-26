13f6922a780f50011a61cc06ea2dc696bf67acc3
Fezao187 <katlegomaredi9@gmail.com>
2024-11-26 06:21:42 +0200

    Backend api

---
+++ tutorials/tut2_fullstacl_app/movies/src/main/java/katlego/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package katlego.movies.repository;
+
+import katlego.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
