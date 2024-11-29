1230bf33515f59147dcdbcb735ae4421ac15346f
Fezao187 <katlegomaredi9@gmail.com>
2024-11-29 03:07:34 +0200

    Card component

---
+++ project/card_generator/client/card_generator_frontend/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({ cards }) => {
+    return (
+        <div>
+            <p><strong>Name: </strong></p>
+            <p><strong>Card Number: </strong></p>
+            <p><strong>Expiery Date: </strong></p>
+            <p><strong>CVV: </strong></p>
+        </div>
+    )
+}
+
+export default Card
